5e30a397904d8b9df51d36875779ccd849dd4868
lefake <lefake@tutanota.com>
2023-09-25 16:40:05 -0400

    Working Macros and Strips with ESP Client

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ vm = None
 
 MOTORIZED = False
 
-MAP_HW_VM = [0, 2]
+MAP_HW_VM = [0, 1, 2, 4, 5]
+MAP_HW_MACRO = [0, 1, 2, 3, 4, 5, 6, 7]
 
 def valid_response(content):
     res = make_response(str(content), status.HTTP_200_OK)
@@ -63,23 +64,48 @@ def push():
     if 'g' in args.keys():
         gains = args.get('g')[:-1].split(',')
 
-        if len(gains) != len(MAP_HW_VM):
+        if len(gains) > len(MAP_HW_VM):
             return BAD_ARGS
 
-        for i, vId in enumerate(MAP_HW_VM):
-            vm.strip[vId].gain = float(gains[i])
+        for pair in gains:
+            vId, g = pair.split(':')
+            vm.strip[MAP_HW_VM[int(vId)]].gain = float(g)
 
     if 'm' in args.keys():
         mute = int(args.get('m'))
         bins = f'{mute:0{len(MAP_HW_VM)}b}'[::-1]
+
+        if len(bins) != len(MAP_HW_VM):
+            return BAD_ARGS
+
         for i, vId in enumerate(MAP_HW_VM):
             if bins[i] == "1":
                 vm.strip[vId].mute = not vm.strip[vId].mute
 
     return valid_response("")
 
+@app.route('/pullMacro', methods=['GET'])
+def pullMacro():
+    return valid_response("")
+
+@app.route('/pushMacro', methods=['POST'])
+def pushMacro():
+    args = request.args
+
+    if 'm' in args.keys():
+        macros = int(args.get('m'))
+        bins = f'{macros:0{len(MAP_HW_MACRO)}b}'[::-1]
+
+        if len(bins) != len(MAP_HW_MACRO):
+            return BAD_ARGS
+
+        for i, mId in enumerate(MAP_HW_MACRO):
+            if bins[i] == "1":
+                print(f"Macro {mId} toggled")
+
+    return valid_response("")
+
 if __name__ == "__main__":
     with voicemeeterlib.api("potato") as VM:
         vm = VM
-        vm.vm_pdirty()
         app.run(host='192.168.2.13', port=5000)
